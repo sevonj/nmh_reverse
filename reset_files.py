0e93f9abe4a3953486ae749db2b1a1617d4af2e9
sevonj <100710152+sevonj@users.noreply.github.com>
2024-08-01 22:19:20 +0300

    reset_files

--- reset_files.py
+++ reset_files.py
@@ -5,9 +5,13 @@ def reset_files():
     """
     replaces ./filesystem_edited/ with a fresh copy.
     """
-
+    print("reset: rmtree")
     shutil.rmtree("filesystem_edited", ignore_errors=True)
+
+    print("reset: copytree")
     shutil.copytree("filesystem", "filesystem_edited")
+
+    print("reset: done!\n")
 
 
 if __name__ == "__main__":
