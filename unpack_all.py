049f8e838cf0d99dfe96b1ed8bd169079b2605f0
sevonj <100710152+sevonj@users.noreply.github.com>
2024-08-08 00:02:09 +0300

    unpack_all all files

--- unpack_all.py
+++ unpack_all.py
@@ -6,53 +6,99 @@ from glob import glob
 
 TOOL_NAME = "Jyl's FLCG exporter"
 
-DIR = "filesystem/DATA/files/STG_HI"
-OUT_DIR = "out/STG_HI"
+GAMEPATH = "filesystem/DATA/files"
+OUTPATH = "out"
 
 
-def unpack_all(dir: str, out_dir: str):
+def unpack_all(in_dir: str, out_dir: str):
+    # rm old files
+    shutil.rmtree(out_dir, ignore_errors=True)
+
     # out dir exists
     os.makedirs(out_dir, exist_ok=True)
 
-    # rm old files
-    shutil.rmtree(OUT_DIR, ignore_errors=True)
+    # Walk the entire game dir
+    for root, _, files in os.walk(in_dir):
+        for filename in files:
 
-    # convert all
-    for file in os.listdir(dir):
-        path = os.path.join(dir, file)
-        subdir = os.path.join(out_dir, file)
+            inputfile = os.path.join(root, filename)
+            relpath = os.path.relpath(inputfile, in_dir)
+            outputdir = os.path.join(out_dir, relpath)
 
-        magic = None
-        with open(path, "rb") as f:
-            magic = f.read(4)
+            magic = None
+            with open(inputfile, "rb") as f:
+                magic = f.read(4)
 
-        if file.lower().endswith(".txt"):
-            print(f"Skipped text file {path}")
-            continue
+            # -- Decide what to do with the file
 
-        match magic:
-            case b"FLCG":
-                os.makedirs(subdir)
-                try:
-                    unpack_flcg.unpack(path, subdir)
-                except Exception as e:
-                    print(f"FAILED - {path}")
-                    with open(os.path.join(subdir, "_error_log.txt"), "w") as error_log:
-                        error_log.write(f"--- I HAVE FAILED ---\n\nException:\n")
-                        error_log.write(f"{e}")
+            if filename.lower().endswith(".txt"):
+                continue
 
-            case b"GMF2":
-                try:
-                    unpack_gmf2.unpack(path, subdir)
-                except Exception as e:
-                    print(f"FAILED - {path}")
-                    with open(os.path.join(subdir, "_error_log.txt"), "w") as error_log:
-                        error_log.write(f"--- I HAVE FAILED ---\n\nException:\n")
-                        error_log.write(f"{e}")
+            if filename.lower().endswith(".bat"):
+                continue
 
-            case _:
-                print(f"unknown id: {magic} - {path}")
+            if filename.lower().endswith(".tga"):
+                continue
+
+            if filename.lower().endswith(".rsid"):
+                continue
+
+            match magic:
+
+                case b"FLCG":
+                    os.makedirs(outputdir)
+                    try:
+                        unpack_flcg.unpack(inputfile, outputdir)
+                    except Exception as e:
+                        print(f"FAILED - {inputfile}")
+                        log_error(e, outputdir)
+
+                case b"GAN2":
+                    pass
+
+                case b"GCT0":
+                    pass
+
+                case b"GMF2":
+                    try:
+                        unpack_gmf2.unpack(inputfile, outputdir)
+                    except Exception as e:
+                        print(f"FAILED - {inputfile}")
+                        log_error(e, outputdir)
+
+                case b"RMHG":
+                    pass
+
+                case b"RSAR":
+                    pass
+
+                case b"RSTM":
+                    pass
+
+                case b"SEST":
+                    pass
+
+                case b"STMD":
+                    pass
+
+                case b"STSD":
+                    pass
+
+                case b"STRI":  # STRIMAG2
+                    pass
+
+                case b"THP\x00":
+                    pass
+
+                case _:
+                    print(f"unknown id: {magic} - {inputfile}")
+
+
+def log_error(e: Exception, outputdir: str):
+    with open(os.path.join(outputdir, "_error_log.txt"), "w") as error_log:
+        error_log.write(f"--- I HAVE FAILED ---\n\nException:\n")
+        error_log.write(f"{e}")
 
 
 if __name__ == "__main__":
-    unpack_all(DIR, OUT_DIR)
+    unpack_all(GAMEPATH, OUTPATH)
